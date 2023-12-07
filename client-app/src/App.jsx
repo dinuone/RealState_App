77a211a29c15010b9d6126b4b0e8976107e95b28
dinuone <dinu11one@gmail.com>
2023-12-08 00:06:47 +0530

    first commit

---
+++ client-app/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Signin from './Pages/Signin'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import Header from './compoenents/Header'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-out" element={<SignUp />} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile/>} />
+    </Routes>
+  </BrowserRouter>
+  )
+ 
+}
