74e09361bc29ca91f8b1f6739ce7ec8181c0f61e
dinuone <dinu11one@gmail.com>
2023-12-08 21:28:38 +0530

    middleware and error handling

--- API/Controllers/Auth.controller.js
+++ API/Controllers/Auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../Models/User.model.js";
 import bycryptjs from 'bcryptjs'
 
-export const Signup = async (req, res) => {
+export const Signup = async (req, res, next) => {
 
   const {username, email, password} = req.body;
   const hashPassword = bycryptjs.hashSync(password,10);
@@ -12,7 +12,7 @@ export const Signup = async (req, res) => {
     res.status(200).json({message:"User Created successfully"})
   
   }catch(err){
-    res.status(422).json(err.message)
+    next(err);
   }
 
  
