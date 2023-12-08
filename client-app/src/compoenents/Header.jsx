8ba16407d8cb9d3d64cb7153eb67c1fd34b72bd2
dinuone <dinu11one@gmail.com>
2023-12-08 21:39:29 +0530

    complete sign up page UI

--- client-app/src/compoenents/Header.jsx
+++ client-app/src/compoenents/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
 
-            <Link to="/sign-in">
+            <Link to="/sign-up">
             <li className='text-slate-700 hover:underline'>Sign In</li>
             </Link>
         </ul>
